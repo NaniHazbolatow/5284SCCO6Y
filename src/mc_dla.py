d1d050f7a4de7dbe43dc18b1d7223a09fa2247f8
kbverlaan <kbverlaan@gmail.com>
2025-03-09 16:32:19 +0100

    update markdown

--- src/mc_dla.py
+++ src/mc_dla.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 def box_counting(grid):
     """Compute the fractal dimension using the box-counting method."""
